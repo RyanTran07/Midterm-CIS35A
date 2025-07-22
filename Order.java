80b5a7571381eff336a669f82f3ae1814a647fa0
RyanTran07 <rtran934@gmail.com>
2025-07-22 16:16:58 -0700

    Initial commit

---
+++ Order.java
@@ -0,0 +1,9 @@
+public class Order {
+
+    private String[] burgerNames = {"De Anza Burger", "Bacon Cheese", "Mushroom Swiss", "Western Burger", "Don Cali Burger"};
+    private int[] orderArr = {0, 0, 0, 0, 0};
+    private final double[] priceArr = {5.25, 5.75, 5.95, 5.95, 5.95};
+    private int studentType;
+
+
+}
