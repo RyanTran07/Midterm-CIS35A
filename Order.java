1c59568cec768d64d275745f89a38e2fc062a466
RyanTran07 <rtran934@gmail.com>
2025-07-22 16:22:17 -0700

    Added display menu and instance variables

--- Order.java
+++ Order.java
@@ -1,9 +1,23 @@
 public class Order {
 
+    private final int arrSize = 5;
     private String[] burgerNames = {"De Anza Burger", "Bacon Cheese", "Mushroom Swiss", "Western Burger", "Don Cali Burger"};
     private int[] orderArr = {0, 0, 0, 0, 0};
     private final double[] priceArr = {5.25, 5.75, 5.95, 5.95, 5.95};
     private int studentType;
 
 
+    public Order() {
+        this.studentType = -1;
+    }
+
+    public void displayMenu() {
+
+        for(int i = 0; i < arrSize; i++) {
+            System.out.println((i+1) + ". " + burgerNames[i] + "- $" + priceArr[i]);
+        }
+
+    }
+
+    
 }
