28484e767ccb24d3b0ae77bdc42fa409b9a67ee0
RyanTran07 <rtran934@gmail.com>
2025-07-22 16:43:00 -0700

    implemented getInputs

--- Driver.java
+++ Driver.java
@@ -1,3 +1,6 @@
 public class Driver {
-
+    public static void main(String[] args) {
+        Order order1 = new Order();
+        order1.displayMenu();
+    }
 }
--- Order.java
+++ Order.java
@@ -1,14 +1,27 @@
+import java.util.Scanner;
+
 public class Order {
 
+    // Array variables
     private final int arrSize = 5;
     private String[] burgerNames = {"De Anza Burger", "Bacon Cheese", "Mushroom Swiss", "Western Burger", "Don Cali Burger"};
     private int[] orderArr = {0, 0, 0, 0, 0};
     private final double[] priceArr = {5.25, 5.75, 5.95, 5.95, 5.95};
-    private int studentType;
+
+    // customerType = 1 (student) customerType = 2 (staff)
+    private int customerType;
+
+    //
+    private double subTotal;
+    private double total;
+    private double tax
+    Scanner input = new Scanner(System.in);
 
 
     public Order() {
         this.studentType = -1;
+        this.subTotal = 0.0;
+        this.total = 0.0;
     }
 
     public void displayMenu() {
@@ -19,5 +32,95 @@ public class Order {
 
     }
 
-    
+
+    public boolean orderArrEmpty() {
+        for(int i = 0; i < arrSize; i++) {
+            if(orderArr[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void getInputs() {
+        boolean orderEnded = false;
+
+        int userBurgerSelection = -1;
+        int userQuantity = -1;
+
+        while(!orderEnded) {
+            displayMenu();
+            System.out.print("Please enter a burger option (1-5), or select 6 to exit: ");
+            userBurgerSelection = input.nextInt();
+
+
+            // Input validation for the burger selection
+            while(userBurgerSelection < 1 || userBurgerSelection > 6) {
+                System.out.print("Please enter a valid burger selection (1-5), or exit (6): ");
+                userBurgerSelection = input.nextInt();
+            }
+
+            // If the user ends the order, end the while loop
+            if(userBurgerSelection == 6) {
+                System.out.println("Your order has now ended.");
+                orderEnded = true;
+                continue;
+            }
+
+            // Collecting input
+            System.out.print("Please enter the number of " + burgerNames[userBurgerSelection - 1] + "that you would like to order: " );
+            userQuantity = input.nextInt();
+
+            // Input validation for the amount of burgers to order
+            while(userQuantity < 0) {
+                System.out.print("Please enter a valid (non-negative) number of burgers to order: ");
+                userQuantity = input.nextInt();
+            }
+
+
+            switch(userBurgerSelection) {
+                case 1:
+                    orderArr[0] += userQuantity;
+                    break;
+                case 2:
+                    orderArr[1] += userQuantity;
+                    break;
+                case 3:
+                    orderArr[2] += userQuantity;
+                    break;
+                case 4:
+                    orderArr[3] += userQuantity;
+                    break;
+                case 5:
+                    orderArr[4] += userQuantity;
+                    break;
+
+            }
+
+
+            System.out.println("1. Student\n2.Staff");
+            System.out.print("Please enter the type of customer you are: ");
+
+        }
+
+
+    }
+
+    public void printBill() {
+
+        for (int i = 0; i < arrSize; i++) {
+            if (orderArr [i] > 0) {
+                System.out.println(orderArr [i] + " " + burgerNames [i] + ": $" + String.format("%.2f", orderArr[i]*priceArr[i]));
+            } else {
+                continue;
+            }
+        }
+        System.out.printf("The total before tax is: $%.2f%n", subTotal);
+        System.out.printf("The tax amount is: $%.2f%n", tax);
+        System.out.printf("The total bill is: $%.2f%n", total);
+
+    }
+
+
 }
