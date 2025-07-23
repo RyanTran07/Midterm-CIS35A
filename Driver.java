e317ab96be2fe7cb76ce2cf83d6a1760838d74b7
RyanTran07 <rtran934@gmail.com>
2025-07-22 20:31:37 -0700

    .

--- Driver.java
+++ Driver.java
@@ -24,6 +24,6 @@ public class Driver {
 }
 
 /*
-    
+    Sample Output 1:
 
  */
--- Order.java
+++ Order.java
@@ -18,7 +18,7 @@ public class Order {
 
     // Array instance variables
     private final int arrSize = 5;
-    private String[] burgerNames = {"De Anza Burger", "Bacon Cheese", "Mushroom Swiss", "Western Burger", "Don Cali Burger"};
+    private String[] burgerNames = {"De Anza Burger", "Bacon Cheese Burger", "Mushroom Swiss Burger", "Western Burger", "Don Cali Burger"};
     private int[] orderArr = {0, 0, 0, 0, 0};
     private final double[] priceArr = {5.25, 5.75, 5.95, 5.95, 5.95};
 
