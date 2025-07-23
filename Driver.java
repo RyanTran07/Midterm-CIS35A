7d19faf6ed86c6866521f8d9fa8e18bb62abef74
RyanTran07 <rtran934@gmail.com>
2025-07-22 17:03:27 -0700

    Input validation for the getInputs() method

--- Driver.java
+++ Driver.java
@@ -1,6 +1,10 @@
 public class Driver {
     public static void main(String[] args) {
         Order order1 = new Order();
+        
         order1.displayMenu();
+        order1.getInputs();
+        order1.calculate();
+        order1.printBill();
     }
 }
--- Order.java
+++ Order.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Order {
 
-    // Array variables
+    // Array instance variables
     private final int arrSize = 5;
     private String[] burgerNames = {"De Anza Burger", "Bacon Cheese", "Mushroom Swiss", "Western Burger", "Don Cali Burger"};
     private int[] orderArr = {0, 0, 0, 0, 0};
@@ -26,14 +26,17 @@ public class Order {
     }
 
     public void displayMenu() {
-
         for(int i = 0; i < arrSize; i++) {
             System.out.println((i+1) + ". " + burgerNames[i] + "- $" + priceArr[i]);
         }
-
     }
 
 
+    /*
+     * boolean orderArrEmpty()
+     *
+     *
+     */
     public boolean orderArrEmpty() {
         for(int i = 0; i < arrSize; i++) {
             if(orderArr[i] != 0) {
@@ -44,6 +47,17 @@ public class Order {
         return true;
     }
 
+    /*
+     * void getInputs()
+     *  Purpose: Collect user input on what burgers should be ordered, how many
+     *           of each burger should be collected, and the type of customer
+     *           the user is.
+     *
+     *  Receives: N/A
+     *  Returns: N/A
+     *  Output: N/A
+     *
+     */
     public void getInputs() {
         boolean orderEnded = false;
 
@@ -53,14 +67,22 @@ public class Order {
         while(!orderEnded) {
             displayMenu();
             System.out.print("Please enter a burger option (1-5), or select 6 to exit: ");
-            userBurgerSelection = input.nextInt();
-
 
             // Input validation for the burger selection
-            while(userBurgerSelection < 1 || userBurgerSelection > 6) {
-                System.out.print("Please enter a valid burger selection (1-5), or exit (6): ");
-                userBurgerSelection = input.nextInt();
+            while (true) {
+            	if (input.hasNextInt()) {
+            		userBurgerSelection = input.nextInt();
+            		if (userBurgerSelection <=6 && userBurgerSelection >= 1) {
+            			break;
+            		} else {
+            			System.out.print("Invalid Selection. Please choose a burger by entering a number from 1-5: ");
+            		}
+            	} else {
+            		input.next();
+            		System.out.print("Invalid Selection. Please choose a burger by entering a number from 1-5: ");
+            	}
             }
+
 
             // If the user ends the order, end the while loop
             if(userBurgerSelection == 6) {
@@ -71,14 +93,23 @@ public class Order {
 
             // Collecting input
             System.out.print("Please enter the number of " + burgerNames[userBurgerSelection - 1] + "that you would like to order: " );
-            userQuantity = input.nextInt();
+
 
             // Input validation for the amount of burgers to order
-            while(userQuantity < 0) {
-                System.out.print("Please enter a valid (non-negative) number of burgers to order: ");
-                userQuantity = input.nextInt();
-            }
 
+            while (true) {
+            	if (input.hasNextInt())  {
+            		userQuantity = input.nextInt();
+            		if (userQuantity>=0) {
+            			break;
+            		} else {
+            			System.out.print("Please enter a valid amount of burgers to order: ");
+            		}
+            	} else {
+            		input.next();
+            		System.out.print("Please enter a valid amount of burgers to order: ");
+            	}
+            }
 
             switch(userBurgerSelection) {
                 case 1:
@@ -103,21 +134,27 @@ public class Order {
             System.out.println("1. Student\n2.Staff");
             System.out.print("Please enter the type of customer you are: ");
 
-            customerType = input.nextInt();
 
-            while(customerType < 1 || customerType > 2) {
-                System.out.print("Please choose a valid customer type (1-2): ");
-                customerType = input.nextInt();
+            while (true) {
+            	if (input.hasNextInt()) {
+            		customerType = input.nextInt();
+            		if (customerType <= 2 && customerType >=1) {
+            			break;
+            		} else {
+            			System.out.print("Please enter a valid customer type. 1 for student or 2 for staff: ");
+            		}
+            	} else {
+            		input.next();
+            		System.out.print("Please enter a valid customer type. 1 for student or 2 for staff: ");
+            	}
             }
-
 
         }
 
-
     }
 
-    public void printBill() {
 
+    public void printBill() {
         for (int i = 0; i < arrSize; i++) {
             if (orderArr [i] > 0) {
                 System.out.println(orderArr [i] + " " + burgerNames [i] + ": $" + String.format("%.2f", orderArr[i]*priceArr[i]));
